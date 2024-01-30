8ff20ae27e4aff1fb6f743765aea7e7b892754da
Ijaz Bacha <ijazbacha14@gmail.com>
2024-01-30 22:17:00 +0500

    create a uploadOnCloudinary method to upload files on cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET 
+});
+
+const uploadOncloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+       const response = await cloudinary.uploader.upload(localFilePath, { resource_type:'auto' })
+        return response
+    } catch (error) {
+        console.log("CLOUDINARY UPLOADER ERROR: ", error);
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {uploadOncloudinary}
